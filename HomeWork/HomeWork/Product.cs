b27c28fce11e49cdabe9cefd5032e4c206172d35
Elshan Rasullu <booqeyman89@gmail.com>
2024-07-16 23:58:13 +0400

    Add files via upload
    1ci ev tapshirigi

---
+++ HomeWork/HomeWork/Product.cs
@@ -0,0 +1,54 @@
+﻿namespace HomeWork
+{
+    public class Canli
+    {
+        public string Deri { get; set; }
+        public bool NefesAlir { get; set; } = true;
+        public bool AyagiVar { get; set; }
+        public string Artir { get; set; }
+
+        public string GetDescription()
+        {
+            string description = $"Canlinin Dərisi: {Deri} -dur";
+            description += $", Nəfəs alır ";
+            if (!string.IsNullOrEmpty(Artir))
+            {
+                description += $", {Artir} artır";
+            }
+            description += AyagiVar ? " yeriyərək hərəkət edir" : " Sürünərək hərəkət edir";
+            return description;
+        }
+
+    }
+    public class Insan : Canli
+    {
+        public string Name { get; set; }
+        public string Dil { get; set; }
+        public string Ixtisas { get; set; }
+
+
+        public new string GetDescription()
+        {
+            string description = $"Canlinin Dərisi: {Deri} ";
+            description += $", Nəfəs alır ";
+            if (!string.IsNullOrEmpty(Artir))
+            {
+                description += $", {Artir}  artır";
+            }
+            description += AyagiVar ? " yeriyərək hərəkət edir" : " Sürünərək hərəkət edir";
+
+            description += $", Adi {Name} dir  ";
+            description += $", {Dil} Dilində danışır";
+            description += $", Peşəsi: {Ixtisas}-dir";
+            if ((Ixtisas) == "IT")
+            {
+                description += $", və Kolleqadır";
+            }
+            return description;
+        }
+        public new string GetDescription(string Ders)
+        {
+            return $"{Ders} yazmag lazimdir";
+        }
+    }
+}
---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,21 @@
+﻿using HomeWork;
+using System.Collections.Specialized;
+
+var canli1 = new Canli();
+canli1.Artir = "yumurtlayaraq";
+canli1.Deri = "pullu";
+canli1.AyagiVar = true;
+
+var canli2 = new Insan()
+{
+    Artir = "dogaraq",
+    Deri = "qismən tüklü",
+    AyagiVar = true,
+    Name = "MAMED",
+    Dil = "Azərbaycan",
+    Ixtisas = "IT"
+};
+
+Console.WriteLine(canli1.GetDescription());
+Console.WriteLine(canli2.GetDescription());
+Console.WriteLine(canli2.GetDescription("Dotnet"));
